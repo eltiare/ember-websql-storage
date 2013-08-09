08e4486e29410cd545d6eab83a5b83a40570d89b
Jeremy Nicoll <jnicoll@accentuate.me>
2013-08-09 11:40:35 -0600

    Uses promises for querying, adds findAll and findQuery.

--- ember-cordova-storage.js
+++ ember-cordova-storage.js
@@ -3,13 +3,6 @@
   @submodule data-adapters
 */
 
-// Placeholder for later use. May just switch to JSONSerializer if customization not needed.
-DS.CordovaStorageSerializer = DS.JSONSerializer.extend({
-  /*init: function() {
-    this._super.apply(this, arguments);
-  }*/
-});
-
 /**
   The Cordova Storage adapter enables the use of permanent storage in your mobile applications. Column names match the
   attribute names exactly as they are defined. The `id` is expected to be present for document retrieval and saving.
@@ -21,14 +14,13 @@ DS.CordovaStorageSerializer = DS.JSONSerializer.extend({
   @extends DS.Adapter
 */
 
-
 DS.CordovaStorageAdapter = DS.Adapter.extend({
 
   dbName: 'cordovadb',
   dbVersion: '1.0',
   dbDisplayName: 'Cordova DB',
   dbSize: 5000000,   // Size is determined in bytes
-  serializer: DS.CordovaStorageSerializer,
+  serializer: DS.JSONSerializer,
 
   init: function() {
     this._super.apply(this, arguments);
@@ -38,23 +30,48 @@ DS.CordovaStorageAdapter = DS.Adapter.extend({
   find: function(store, type, id) {
     var qr = new QueryRapper({id: id}).tableName(this.tableName(type));
     var adapter = this;
-    function selectSuccess(tx, results) {
-      var data, root = this.rootForType(type);
-      data = {};
-      data[root] = {};
-      if (results.rows.length > 0) {
-        data[root] = results.rows.item(0);
-        adapter.didFindRecord(store,type,data,id);
-      }
-    }
-    var query = qr.selectQuery();
-    this.db.transaction(
-      function(tx) { tx.executeSql(query, [], selectSuccess); },
-      function(err) {
-        adapter.logError(query);
-        adapter.rejectionHandler(err);
-      }
-    )
+    return this.query(qr.selectQuery())
+        .then(function(arr) {
+          var tx = arr[0], results = arr[1];
+          var data = {}, root = adapter.rootForType(type);
+          data[root] = {};
+          if (results.rows.length > 0) {
+            data[root] = results.rows.item(0);
+            adapter.didFindRecord(store,type,data,id);
+          } else {
+            throw new Error('Record was not found');
+          }
+        })
+        .then(null, DS.rejectionHandler);
+  },
+
+  findAll: function(store, type) {
+    var qr = new QueryRapper().tableName(this.tableName(type));
+    var adapter = this;
+    return this.query(qr.selectQuery())
+        .then(function(arr) {
+          var tx = arr[0], results = arr[1];
+          var data = {}, root = adapter.pluralize(type);
+          data[root] = [];
+          for (var i = 0; i < results.rows.length; i++) { data.push(results.rows.item(i)); }
+          adapter.didFindAll(store,type,data);
+        })
+        .then(null, DS.rejectionHandler);
+  },
+
+  findQuery: function(store, type, query, recordArray) {
+    var root = this.rootForType(type),
+        adapter = this;
+    var qr = new QueryRapper(query).tableName(this.tableName(type));
+    return this.query(qr.selectQuery())
+        .then(function(arr) {
+          var tx = arr[0], results = arr[1];
+          var data = {}, root = adapter.rootForType(type);
+          data[root] = [];
+          for (var i = 0; i < results.rows.length; i++) { data.push(results.rows.item(i)); }
+          adapter.didFindQuery(store,type,data,recordArray);
+        })
+        .then(null, DS.rejectionHandler);
   },
 
   createRecord: function(store, type, record) {
@@ -65,16 +82,20 @@ DS.CordovaStorageAdapter = DS.Adapter.extend({
     var adapter = this;
     var qr = new QueryRapper().tableName(this.tableName(type)).values(data);
     var query = qr.insertQuery();
-    function insertSuccess(tx, results) {
-      data.id = results.insertId;
-      var dat = {}, root = adapter.rootForType(type);
-      dat[root] = data;
-      adapter.didCreateRecord(store, type, record, dat);
-    }
-    return this.db.transaction(
-      function(tx) { tx.executeSql(query, [], insertSuccess); },
-      function(err) { this.dbError(query, err, type, record); }
-    );
+    return this.query(query)
+        .then(
+          function(arr) {
+            var tx = arr[0], results = arr[1];
+            data.id = results.insertId;
+            var dat = {}, root = adapter.rootForType(type);
+            dat[root] = data;
+            adapter.didCreateRecord(store, type, record, dat);
+          },
+          function(err) {
+            adapter.dbError(store, query, err, type, record);
+            throw err;
+          }
+        ).then(null, DS.rejectionHandler);
   },
 
   updateRecord: function(store, type, record) {
@@ -85,32 +106,32 @@ DS.CordovaStorageAdapter = DS.Adapter.extend({
     var adapter = this;
     var qr = new QueryRapper({id: record.id}).tableName(this.tableName(type)).values(data);
     var query = qr.updateQuery();
-    function updateSuccess(tx, results) {
-      var dat = adapter.serializeWithRootAndId(type, record);
-      adapter.didUpdateRecord(store, type, record, dat);
-    }
-    return this.db.transaction(
-      function(tx) { tx.executeSql(query, [], updateSuccess); },
-      function(err) { this.dbError(query, err, type, record); }
-    )
+    return this.query(query)
+        .then(
+          function(tx, results) {
+            var dat = adapter.serializeWithRootAndId(type, record);
+            adapter.didUpdateRecord(store, type, record, dat);
+          },
+          function(err) {
+            adapter.dbError(store, query, err, type, record);
+          }
+        ).then(null, DS.rejectionHandler);
   },
 
   deleteRecord: function(store, type, record) {
     var adapter = this;
     var qr = new QueryRapper({id: record.get('id')}).tableName(this.tableName(type));
     var query = qr.deleteQuery();
-    function deleteSuccess(tx, results) {
-      adapter.didDeleteRecord(store,type,record);
-    }
-    return this.db.transaction(
-      function(tx) { tx.executeSql(query, [], deleteSuccess); },
-      function(err) { this.dbError(query, err, type, record); }
-    );
+    return this.query(query)
+        .then(
+          function(tx, results) { adapter.didDeleteRecord(store,type,record); },
+          function(err) { adapter.dbError(store, query, err, type, record); }
+    ).then(null, DS.rejectionHandler);
   },
 
 
   ///////////// Support Functions //////////////////
-  dbError: function(query, err, type, record) {
+  dbError: function(store, query, err, type, record) {
     this.logError(query, err, record);
     record.errors = record.errors || {};
     record.errors.db = err.message;
@@ -144,10 +165,14 @@ DS.CordovaStorageAdapter = DS.Adapter.extend({
     }
   },
 
-  rejectionHandler: function(reason) {
-    Ember.Logger.error(reason, reason.message);
-    throw reason;
+  query: function(query) {
+    var adapter = this;
+    return new Ember.RSVP.Promise(function(resolve, reject){
+      adapter.db.transaction(
+          function(tx)  { tx.executeSql(query, [], function(tx, results) { Ember.run(null, resolve, [tx, results]); }); },
+          function(err) { adapter.logError(query); Ember.run(null, reject, err); }
+      );
+    });
   }
-
 
 });
